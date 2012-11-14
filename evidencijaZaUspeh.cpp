cf0cea83406673ac1d2e552260e1a8a47a6b5dbb
hgeshoski <kikogesoski@yahoo.com>
2012-11-14 19:14:15 +0100

    dodadena e nova granka "granka2"

--- evidencijaZaUspeh.cpp
+++ evidencijaZaUspeh.cpp
@@ -11,6 +11,109 @@ struct student
        float prosek;
 };
 
+void sortiranje(struct student* niza,int vel)
+{
+     int i,j;
+     for(i=0;i<(vel-1);i++)
+     for(j=i+1;j<vel;j++)
+     {
+     int prez=strcmp(niza[i].prezime,niza[j].prezime);
+     int ime=strcmp(niza[i].ime,niza[j].ime);
+     if(prez>0 ||(prez==0 && ime>0))
+     {
+     struct student priv=niza[i];
+     niza[i]=niza[j];
+     niza[j]=priv;
+     }
+     }
+}
+
+void vnes(struct student* niza, int* vel)
+{
+     struct student a;
+     if(*vel>=MAXVEL)
+     {
+     printf("Nema mesto!\n");
+     return;
+     }
+     printf("\n Vnesi ime: ");
+     scanf("%s", a.ime);
+     printf(" Vnesi prezime: ");
+     scanf("%s", a.prezime);
+     printf(" Vnesi broj na indeks: ");
+     scanf("%d", &a.indeks);
+     printf("Vnesi ocenki:\n");
+     
+{
+     do
+     {
+     printf(" Voved vo Programiranje: ");
+     scanf("%d", &a.vop);
+     if(a.vop<5 || a.vop>10)
+     printf("Ocenkata ne e vo opseg 5-10\n");
+     else
+     break;
+     }
+     while(1);
+     do
+     {
+     printf(" Elektrotehnika: ");
+     scanf("%d", &a.elektro);
+     if(a.elektro<5 || a.elektro>10)
+     printf("Ocenkata ne e vo opseg 5-10\n");
+     else
+     break;
+     }
+     while(1);
+     do
+     {
+     printf(" Matematika: ");
+     scanf("%d", &a.mat);
+     if(a.mat<5 || a.mat>10)
+     printf("Ocenkata ne e vo opseg 5-10\n");
+     else
+     break;
+     }
+     while(1);
+     do
+     {        
+     printf(" Stranski Jazik: ");
+     scanf("%d", &a.jazik);
+     if(a.jazik<5 || a.jazik>10)
+     printf("Ocenkata ne e vo opseg 5-10\n");
+     else
+     break;
+     }
+     while(1);
+     do
+     {
+     printf(" Aplikativen Softver: ");
+     scanf("%d", &a.aplikativen);     
+     if(a.aplikativen<5 || a.aplikativen>10)
+     printf("Ocenkata ne e vo opseg 5-10\n");
+     else
+     break;
+     }
+     while(1);
+}
+     int temp=0;
+     if(a.aplikativen>5)
+          temp = temp + a.aplikativen;
+     if(a.vop>5)
+          temp = temp + a.vop;
+     if(a.mat>5)
+          temp = temp + a.mat;
+     if(a.elektro>5)
+          temp = temp + a.elektro;
+     if(a.jazik>5)
+          temp = temp + a.jazik;
+     a.prosek = (float) temp/5;
+     printf("Studentot ima prosek: %2.2f", a.prosek);
+     niza[(*vel)++]=a;
+     sortiranje(niza,*vel);
+     printf("\n");
+     }
+   
 int main(int argc, char *argv[])
 {
     
