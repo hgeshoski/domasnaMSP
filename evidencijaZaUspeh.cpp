e76cf8881601a495b87ade080fba0022d91470cd
hgeshoski <kikogesoski@yahoo.com>
2012-11-14 19:11:46 +0100

    prv commit na evidencijaZaUspeh.cpp proektot

---
+++ evidencijaZaUspeh.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#define MAXVEL 100
+using namespace std;
+struct student
+{
+       char ime[15];
+       char prezime[10];
+       int indeks;
+       int vop,aplikativen,elektro,mat,jazik;
+       float prosek;
+};
+
+int main(int argc, char *argv[])
+{
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
